42af83247af26e0908bba8e474fbd738f67d048d
yash-batavle-optimaio <developer@optimaio.com>
2025-11-25 15:34:15 +0530

    testing token demo console log

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,7 +1,8 @@
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
+console.log("App installed for shop Before");
   await authenticate.admin(request);
-
+console.log("App installed for shop After");
   return null;
 };
