2bc09b23c27ac01408e5a1e77640400d6125f6b4
yash-batavle-optimaio <developer@optimaio.com>
2026-01-12 15:45:15 +0530

    billling webhook

---
+++ app/routes/webhooks.app.create_subscription.jsx
@@ -0,0 +1,53 @@
+import { authenticate } from "../shopify.server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const loader = () =>
+  Response.json({ message: "👋 Webhook endpoint: POST only." });
+
+export const action = async ({ request }) => {
+  const { topic, payload, session, shop } = await authenticate.webhook(request);
+
+  if (!session) {
+    console.warn("⚠️ No active session found. Shop may have uninstalled.");
+    return new Response();
+  }
+
+  console.log("-------=====Webhook topic received create: subscription", topic);
+  //   if (topic === "CARTS_CREATE") {
+  console.log("🛒 Full Cart Webhook Data:");
+  console.log(JSON.stringify(payload, null, 2));
+
+  // -----------------------------
+  // Extract Analytics Data
+  // -----------------------------
+  const cartId = payload.id;
+  const storeId = shop; // e.g. mystore.myshopify.com
+  const customerId = payload.buyerIdentity;
+
+  console.log("📊 Subscription Data:");
+  console.log("Store ID:", storeId);
+  console.log("Cart ID:", cartId);
+  console.log("Customer ID:", customerId);
+  console.log("Extra Data", JSON.stringify(payload.buyerIdentity, null, 2));
+
+  //     try {
+  //       await prisma.cartEvent.create({
+  //         data: {
+  //           cartId,
+  //           storeId,
+  //           customerId,
+  //           eventType: "CARTS_CREATE",
+  //           payload,
+  //         },
+  //       });
+
+  //       console.log("✅ Saved analytics data to DB! Using CARTS_CREATE webhook.");
+  //     } catch (err) {
+  //       console.error("❌ DB Save Error:", err);
+  //     }
+  //   }
+
+  return new Response("ok");
+};
